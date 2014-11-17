cb345a9f5ab46fe4f45a5ca087acbddbbbdd67d5
Marcus Young <myoung34@my.apsu.edu>
2014-11-17 13:22:01 -0600

    Issue 2: Changing port does not cause a service restart

--- spec/acceptance/hbase_spec.rb
+++ spec/acceptance/hbase_spec.rb
@@ -33,4 +33,34 @@ describe 'hbase class' do
     end
 
   end
+
+  it 'should allow port change' do
+    pp = <<-EOS
+      class { 'hbase':
+        port => '8000',
+      }
+    EOS
+
+    # Run it twice and test for idempotency
+    apply_manifest(pp, :catch_failures => true)
+    expect(apply_manifest(pp, :catch_failures => true).exit_code).to be_zero
+
+    shell("sleep 30 && curl -s http://localhost:8000/version") do |result|
+      assert_match /^rest/, result.stdout, 'HBase REST did not return a version'
+    end
+
+    shell("curl -s -X PUT 'http://localhost:8000/test2/schema' -H 'Accept: application/json' -H 'Content-Type: application/json' -d '{\"@name\":\"test2\",\"ColumnSchema\":[{\"name\":\"data\"}]}'  -w '%{http_code}'") do |result|
+      assert_match result.stdout, '201', 'HBase REST Master create the table.'
+    end
+
+    shell("curl -s -o /tmp/hbase_table_list2.html -w '%{http_code}' http://localhost:8000") do |result|
+      assert_match result.stdout, '200', 'HBase REST Master did not return a 200 OK'
+    end
+
+    shell("cat /tmp/hbase_table_list2.html") do |result|
+      assert_match /^test\ntest2\n?$/, result.stdout, 'HBase REST Master did not return the created table.'
+    end
+
+  end
+
 end
